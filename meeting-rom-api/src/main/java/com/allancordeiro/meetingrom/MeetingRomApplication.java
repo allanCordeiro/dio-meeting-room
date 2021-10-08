35a89d68cb10fcddbc721c89c7d5f954c1b73d8e
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-10-07 21:25:25 -0300

    primeiro commit

---
+++ meeting-rom-api/src/main/java/com/allancordeiro/meetingrom/MeetingRomApplication.java
@@ -0,0 +1,13 @@
+package com.allancordeiro.meetingrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRomApplication.class, args);
+	}
+
+}
---
+++ meeting-rom-api/src/main/java/com/allancordeiro/meetingrom/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.allancordeiro.meetingrom.repository;
+
+import com.allancordeiro.meetingrom.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
